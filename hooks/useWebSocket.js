60219031b4e797121d3f40aa37668f2b4d046f72
Lautaro <lautaro.romczyk@gmail.com>
2024-06-14 15:52:00 -0300

    Improve websocket custom hook

--- hooks/useWebSocket.js
+++ hooks/useWebSocket.js
@@ -1,23 +1,24 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { router } from 'expo-router';
 import { isPaymentCompleted } from '../utils/payments';
 import { fromStringToFloat } from '../utils/stringUtils';
 import { PaymentContext } from '../context/PaymentContext';
+import { AppState } from 'react-native';
 
 const useWebSocket = (screenName) => {
   const { setPaymentDone, paymentDone, setPaymentData, orderId, amount, currency } = useContext(PaymentContext);
 
-  useEffect(() => {
-    if (paymentDone) router.navigate('/payment-done');
-  }, [paymentDone]);
+  const socketRef = useRef(null);
 
-  useEffect(() => {
-    const websocket = new WebSocket(`wss://payments.pre-bnvo.com/ws/merchant/${orderId}`);
+  function openSocket() {
+    console.log(`[${screenName}] Opening socket`);
 
-    websocket.addEventListener('open', () => console.log(`[${screenName}] Socket opened`));
-    websocket.addEventListener('close', () => console.log(`[${screenName}] Socket closed`));
+    socketRef.current = new WebSocket(`wss://payments.pre-bnvo.com/ws/merchant/${orderId}`);
 
-    websocket.addEventListener('message', (event) => {
+    socketRef.current.addEventListener('open', () => console.log(`[${screenName}] Socket opened`));
+    socketRef.current.addEventListener('close', () => console.log(`[${screenName}] Socket closed`));
+
+    socketRef.current.addEventListener('message', (event) => {
       if (!event?.data) return;
 
       console.log(`[${screenName}] event.data:`, event.data);
@@ -33,9 +34,40 @@ const useWebSocket = (screenName) => {
         router.navigate('/payment-done');
       }
     });
+  }
+
+  useEffect(() => {
+    if (paymentDone) router.navigate('/payment-done');
+  }, [paymentDone]);
+
+  useEffect(() => {
+    const socketAlreadyOpen = socketRef.current?.readyState === 1;
+
+    if (!socketAlreadyOpen) {
+      openSocket();
+    }
 
     return () => {
-      websocket.close();
+      socketRef.current?.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleAppStateChange = (nextAppState) => {
+      if (nextAppState === 'active') {
+        console.log('App has come to foreground');
+
+        if (!socketRef.current || socketRef.current.readyState !== 1) {
+          console.log('Socket needs to be reopened');
+          openSocket();
+        }
+      }
+    };
+
+    AppState.isAvailable && AppState.addEventListener && AppState.addEventListener('change', handleAppStateChange);
+
+    return () => {
+      AppState.isAvailable && AppState.removeEventListener && AppState.removeEventListener('change', handleAppStateChange);
     };
   }, []);
 };
